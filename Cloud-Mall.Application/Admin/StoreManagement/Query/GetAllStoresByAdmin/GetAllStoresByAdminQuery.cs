1b7a8553f772e8585da367df2f9ba586f0f8c371
Ahmed Roshdi <ahmedroshdi79@gmail.com>
2025-07-08 21:01:38 +0300

    GetAllStores By Admin

---
+++ Cloud-Mall.Application/Admin/StoreManagement/Query/GetAllStoresByAdmin/GetAllStoresByAdminQuery.cs
@@ -0,0 +1,18 @@
+﻿using Cloud_Mall.Application.DTOs.Store;
+using MediatR;
+
+namespace Cloud_Mall.Application.Admin.StoreManagement.Query.GetAllStoresByAdmin;
+
+public class GetAllStoresByAdminQuery : IRequest<GetAllStoresByAdminWithPaginationDTO>
+{
+    public string? CategoryName { get; set; }
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+    public GetAllStoresByAdminQuery(string? categoryName = null, int pageNumber = 1, int pageSize = 10)
+    {
+        CategoryName = categoryName;
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+    }
+
+}
